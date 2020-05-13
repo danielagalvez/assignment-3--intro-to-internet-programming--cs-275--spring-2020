bce9038757c67a67210c64bbf5d29bf57fab329b
danielagalvez <60191717+danielagalvez@users.noreply.github.com>
2020-05-12 22:50:58 -0400

    Create toggle function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 let tableData = document.querySelectorAll(`td`); // get all td elements
+function toggle(td) {
+    if (selectedTd) { // remove the existing rotation if any
+        selectedTd.classList.remove(`rotate`);
+    }
+
+    selectedTd = td;
     selectedTd.classList.add(`rotate`); // rotate the new data
 }
 
