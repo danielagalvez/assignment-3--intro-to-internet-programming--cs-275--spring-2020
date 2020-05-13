e385a7dc27b7f3b95f13b00191078faea0263d25
danielagalvez <60191717+danielagalvez@users.noreply.github.com>
2020-05-12 22:51:24 -0400

    Create mutual exclusion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 let tableData = document.querySelectorAll(`td`); // get all td elements
+//loop is used so there is no need to write the event listener for each td element. 
+for(let i = 0; i < tableData.length; i++) {
+    tableData[i].addEventListener(`click`, (event) => {
+        let target = event.target; // pass event to target
+        while (target != this) {
+            if (target.tagName == `TD`) {
+                toggle(target); //toggle function rotate 180 deg and back
+                return; 
+            }
+            target = target.parentNode;
+            // makes node which triggered event the parent. 
+        }
+
+
+    } );
+}
 function toggle(td) {
     if (selectedTd) { // remove the existing rotation if any
         selectedTd.classList.remove(`rotate`);
